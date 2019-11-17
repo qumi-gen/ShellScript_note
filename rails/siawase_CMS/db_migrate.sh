b268526a0cb73c9192ab7884a38a697613a06e04
qumi-gen <35165086+qumi-gen@users.noreply.github.com>
2019-11-17 11:29:55 +0900

    Update db_migrate.sh

--- rails/siawase_CMS/db_migrate.sh
+++ rails/siawase_CMS/db_migrate.sh
@@ -1,6 +1,6 @@
-sudo rails g model comment ID:string DATE:date COMMENT:text #db/migrateに左記で定義されたマイグレーションファイルが生成
+rails g model comment ID:string DATE:date COMMENT:text #db/migrateに左記で定義されたマイグレーションファイルが生成
 
-sudo rails g model content_master ID:string name:string
+rails g model content_master ID:string name:string
 
 
 bundle exec rake db:migrate
