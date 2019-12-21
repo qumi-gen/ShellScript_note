3e6e2682a950710feba1fcea3eba8ecea86e7a83
qumi-gen <35165086+qumi-gen@users.noreply.github.com>
2019-12-21 22:55:43 +0900

    Create electron.sh

---
+++ install/npm/electron.sh
@@ -0,0 +1,17 @@
+# electron
+npm install -S electron
+
+# react.js
+npm install -S react react-dom
+
+# TypeScript
+npm install -D typescript
+npm install -D awesome-typescript-loader
+npm install -D source-map-loader
+
+# React.js の型定義ファイル
+npm install -D @types/react
+npm install -D @types/react-dom
+
+# webpack
+npm install -D webpack
